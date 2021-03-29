700c4d57458332cc67df4d3e9aee7470cb5b2e10
Guilherme Crozariol <guicrozariol@gmail.com>
2021-03-29 19:32:40 -0300

    fix typo

--- views/NotificationView.js
+++ views/NotificationView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import styles from './NotificationsView.styles';
+import styles from './NotificationView.styles';
 
 const NotificationsView = () => {
   return (
