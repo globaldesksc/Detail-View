bbcffcee05b972c31abe989df01eec1b21164d13
globaldesksc <globaldesksolutions@gmail.com>
2021-03-29 18:18:17 -0300

    Final change!

--- pages/Detailview.js
+++ pages/Detailview.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 const DetailView = () => {
     return(
